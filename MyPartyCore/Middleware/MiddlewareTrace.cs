05dac3a05c0f9a3c73737747d0a576fada5ec939
p.titov <p.titov@TITOV-P>
2019-10-17 11:44:16 +0300

    -L12.2-P1/2-MiddlewareExport -L12.2-P2/2-MiddlewareTrace.

---
+++ MyPartyCore/Middleware/MiddlewareTrace.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPartyCore.Middleware
+{
+    public class MiddlewareTrace
+    {
+        private readonly RequestDelegate _next;
+        private readonly IHostingEnvironment _env;
+
+        public MiddlewareTrace(RequestDelegate next, IHostingEnvironment env)
+        {
+            this._next = next;
+            this._env = env;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+            string headersRequest = string.Empty;
+            foreach (var header in context.Request.Headers)
+            {
+                headersRequest = headersRequest + $"{header.Key}:  {header.Value}{Environment.NewLine}"; 
+            }
+
+            string path = Path.Combine(_env.WebRootPath, "Log.txt");
+            string logInfoRequest = $"Time: {DateTime.Now} {Environment.NewLine}QueryString: {context.Request.Path.ToString()} {Environment.NewLine}Headers: {headersRequest} {Environment.NewLine}";
+
+            using (StreamWriter fs = new StreamWriter(path, true))
+            {
+                await fs.WriteAsync(logInfoRequest);
+            }
+
+            await _next.Invoke(context);
+
+            string headersResponse = string.Empty;
+            foreach (var header in context.Response.Headers)
+            {
+                headersResponse = headersResponse + $"{header.Key}:  {header.Value}{Environment.NewLine}";
+            }
+            string logInfoResponse = $"Time: {DateTime.Now} {Environment.NewLine}Headers: {headersResponse} {Environment.NewLine}";
+
+            using (StreamWriter fs = new StreamWriter(path, true))
+            {
+                await fs.WriteAsync(logInfoResponse);
+            }
+
+        }
+    }
+
+    public static class MiddlewareTraceExtensions
+    {
+        public static IApplicationBuilder UseMiddlewareTrace(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<MiddlewareTrace>();
+        }
+    }
+}
